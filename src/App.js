7d7d8a4df316a9539b8db0bf305a97afbbfe3f9f
lingardd09 <sherlockicker@gmail.com>
2025-03-24 03:13:32 +0900

    style: header, sidebar, body, etc...

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import avatar from './assets/avatar1.png'
 
 function App() {
   return (
-    <>
+    <div className='app'>
       <div className='header'>
         Logo
       </div>
@@ -52,18 +52,42 @@ function App() {
           </div>
         </div>
         <div className='body'>
-          My Dashboard
-      <div className='footer'>
-        <div className='footer-title'>
-          FOOTER
-        </div>
-        <div className='footer-content'>
-          powered by <a className='w3-css-link' href='https://www.w3schools.com/w3css/default.asp'>w3.css</a>
-        </div>
-      </div>    
+          <div className='body-title'>
+            My Dashboard
+          </div>
+          <div className='dashboard'>
+            <div className='dash-icon'>
+              Messages
+            </div>
+            <div className='dash-icon'>
+              Views
+            </div>
+            <div className='dash-icon'>
+              Shares
+            </div>
+            <div className='dash-icon'>
+              Users
+            </div>
+          </div>
+          <div className='body-infos'>
+            <div className='body-info'>
+              Regions
+            </div>
+            <div className='body-info'>
+              Feeds
+            </div>
+          </div>
+          <div className='footer'>
+            <div className='footer-title'>
+              FOOTER
+            </div>
+            <div className='footer-content'>
+              powered by <a className='w3-css-link' href='https://www.w3schools.com/w3css/default.asp'>w3.css</a>
+            </div>
+          </div>    
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
